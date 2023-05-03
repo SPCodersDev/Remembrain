860d7cc319400a1ed4678e621c9cf05469c5ff2b
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-03 19:09:25 +0000

    Inital Setup Part 1

---
+++ scripts/init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeUGKkDd4CpYQrzeSJF_gEQEHDlsQIGg0",
+    authDomain: "programmingcompetition2023.firebaseapp.com",
+    projectId: "programmingcompetition2023",
+    storageBucket: "programmingcompetition2023.appspot.com",
+    messagingSenderId: "482409458124",
+    appId: "1:482409458124:web:c6472678e76fbd2583d569"
+};
+
+firebase.initializeApp(firebaseConfig);
---
+++ scripts/login.js
@@ -0,0 +1,28 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+    var entity = {
+        id: profile.getId(),
+        name: profile.getName(),
+        image: profile.getImageUrl(),
+        email: profile.getEmail(),
+    }
+
+    localStorage.setItem("entity", JSON.stringify(entity));
+    localStorage.setItem("loggedIn", JSON.stringify(true));
+
+    setData(entity);
+
+    window.setTimeout(function () {
+        window.location.href = "home.html";
+    }, 100);
+}
+
+function setData(entityObj) {
+    firebase.database().ref('users/' + entityObj.id).set({
+        id: entityObj.id,
+        name: entityObj.name,
+        image: entityObj.image,
+        email: entityObj.email,
+    });
+}
