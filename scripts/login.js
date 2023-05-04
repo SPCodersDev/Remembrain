d27299b47289e52ef1a1431dd8b69a414bbec69d
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-04 10:20:42 +0100

    Try New

--- scripts/login.js
+++ scripts/login.js
@@ -18,6 +18,21 @@ function onSignIn = (googleUser) => {
     }, 100);
 }
 
+function handleCredentialResponse(response) {
+          console.log("Encoded JWT ID token: " + response.credential);
+        }
+        window.onload = function () {
+          google.accounts.id.initialize({
+            client_id: "YOUR_GOOGLE_CLIENT_ID",
+            callback: handleCredentialResponse
+          });
+          google.accounts.id.renderButton(
+            document.getElementById("buttonDiv"),
+            { theme: "outline", size: "large" }  // customization attributes
+          );
+          google.accounts.id.prompt(); // also display the One Tap dialog
+        }
+
 function setData(entityObj) {
     firebase.database().ref('users/' + entityObj.id).set({
         id: entityObj.id,
