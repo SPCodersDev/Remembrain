2624cd9222853821dee9953992af21bdd40b8cde
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-07 18:24:58 +0000

    REMOVE GOOHLE OAUTH (ANGRY)

--- scripts/login.js
+++ scripts/login.js
@@ -1,33 +1,12 @@
-function onSignIn(response) {
-
-    // const responsePayload = decodeJwtResponse(response.credential);
-
-    // console.log("ID: " + responsePayload.sub);
-    // console.log('Full Name: ' + responsePayload.name);
-    // console.log('Given Name: ' + responsePayload.given_name);
-    // console.log('Family Name: ' + responsePayload.family_name);
-    // console.log("Image URL: " + responsePayload.picture);
-    // console.log("Email: " + responsePayload.email);
-
-    console.log(response)
-
-    var responsePayload = response.getBasicProfile();
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
 
     var entity = {
-        id: responsePayload.sub,
-        name: responsePayload.name,
-        image: responsePayload.picture,
-        email: presponsePayload.email,
+        id: profile.getId(),
+        name: profile.getName(),
+        image: profile.getImageUrl(),
+        email: profile.getEmail(),
     }
-
-    // var profile = googleUser.getBasicProfile();
-
-    // var entity = {
-    //     id: profile.getId(),
-    //     name: profile.getName(),
-    //     image: profile.getImageUrl(),
-    //     email: profile.getEmail(),
-    // }
 
     localStorage.setItem("entity", JSON.stringify(entity));
     localStorage.setItem("loggedIn", JSON.stringify(true));
