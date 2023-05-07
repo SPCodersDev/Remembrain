c68fb747129bfcd7594c2b6615cd87b414a39bf4
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-07 17:18:39 +0000

    Add DB URL

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,7 @@
 const firebaseConfig = {
     apiKey: "AIzaSyBeUGKkDd4CpYQrzeSJF_gEQEHDlsQIGg0",
     authDomain: "programmingcompetition2023.firebaseapp.com",
+    databaseURL: "https://programmingcompetition2023-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "programmingcompetition2023",
     storageBucket: "programmingcompetition2023.appspot.com",
     messagingSenderId: "482409458124",
