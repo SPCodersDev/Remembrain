d0f1c3b50605f93c363e1d425a820e9b4f816939
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-07 21:32:10 +0000

    WORKING LOGIN SYSTEM

--- scripts/login.js
+++ scripts/login.js
@@ -1,96 +1,47 @@
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
+  var profile = googleUser.getBasicProfile();
 
-    var entity = {
-        id: profile.getId(),
-        name: profile.getName(),
-        image: profile.getImageUrl(),
-        email: profile.getEmail(),
-    }
+  var entity = {
+    id: profile.getId(),
+    name: profile.getName(),
+    image: profile.getImageUrl(),
+    email: profile.getEmail(),
+  }
 
-    localStorage.setItem("entity", JSON.stringify(entity));
-    localStorage.setItem("loggedIn", JSON.stringify(true));
+  localStorage.setItem("entity", JSON.stringify(entity));
+  localStorage.setItem("loggedIn", JSON.stringify(true));
 
-    setData(entity);
+  setData(entity);
 
-    window.setTimeout(function () {
-        window.location.href = "home.html";
-    }, 100);
+  window.setTimeout(function () {
+    window.location.href = "home.html";
+  }, 100);
 }
 
 function setData(entityObj) {
-    firebase.database().ref('users/' + entityObj.id).set({
-        id: entityObj.id,
-        name: entityObj.name,
-        image: entityObj.image,
-        email: entityObj.email,
-    });
+  firebase.database().ref('users/' + entityObj.id).set({
+    id: entityObj.id,
+    name: entityObj.name,
+    image: entityObj.image,
+    email: entityObj.email,
+  });
 }
 
-function signInWithEmailPassword() {
-    var email = "test@example.com";
-    var password = "hunter2";
-    // [START auth_signin_password]
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-      });
-    // [END auth_signin_password]
-  }
-  
-  function signUpWithEmailPassword() {
-    var email = "test@example.com";
-    var password = "hunter2";
-    // [START auth_signup_password]
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Signed in 
-        var user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ..
-      });
-    // [END auth_signup_password]
-  }
-  
-  function sendEmailVerification() {
-    // [START auth_send_email_verification]
-    firebase.auth().currentUser.sendEmailVerification()
-      .then(() => {
-        // Email verification sent!
-        // ...
-      });
-    // [END auth_send_email_verification]
-  }
-  
-  function sendPasswordReset() {
-    const email = "sam@example.com";
-    // [START auth_send_password_reset]
-    firebase.auth().sendPasswordResetEmail(email)
-      .then(() => {
-        // Password reset email sent!
-        // ..
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ..
-      });
-    // [END auth_send_password_reset]
-  }
-// OLD CODE
-//   if (JSON.parse(localStorage.getItem("sign"))) {
-//     window.location.href = "home.html"
+// function sendEmailVerification() {
+//   // [START auth_send_email_verification]
+//   firebase.auth().currentUser.sendEmailVerification()
+//     .then(() => {
+//       // Email verification sent!
+//       // ...
+//     });
+//   // [END auth_send_email_verification]
 // }
+
+
+// OLD CODE
+if (JSON.parse(localStorage.getItem("loggedIn"))) {
+  window.location.href = "home.html"
+}
 
 // if (localStorage.getItem("users") == null) {
 //     var users = [];
@@ -100,82 +51,100 @@ function signInWithEmailPassword() {
 //     var users = JSON.parse(localStorage.getItem("users"));
 // }
 
-// var cont;
+var cont;
 
-// var emailError = document.querySelector(".errorEmail");
-// var passwordError = document.querySelector(".errorPassword");
+var emailError = document.querySelector(".errorEmail");
+var passwordError = document.querySelector(".errorPassword");
 
-// function emailErrorShow(value) {
-//     emailError.style.display = "block";
-//     emailError.innerHTML = value;
-// }
+function emailErrorShow(value) {
+  emailError.style.display = "block";
+  emailError.innerHTML = value;
+}
 
-// function passwordErrorShow(value) {
-//     passwordError.style.display = "block";
-//     passwordError.innerHTML = value;
-// }
+function passwordErrorShow(value) {
+  passwordError.style.display = "block";
+  passwordError.innerHTML = value;
+}
 
-// var loginBtn = document.querySelector(".loginButton");
+var loginBtn = document.querySelector(".loginButton");
 
-// loginBtn.addEventListener("click", function () {
-//     emailError.style.display = "none";
-//     passwordError.style.display = "none";
+loginBtn.addEventListener("click", function () {
+    emailError.style.display = "none";
+    passwordError.style.display = "none";
 
-//     if (document.querySelector(".loginCredentialEmail").value.length != 0) {
-//         cont = true;
-//     } else {
-//         cont = false;
+    if (document.querySelector(".loginCredentialEmail").value.length != 0) {
+        cont = true;
+    } else {
+        cont = false;
 
-//         emailErrorShow("Please Enter Email");
-//     }
+        emailErrorShow("Please Enter Email");
+    }
 
-//     if (document.querySelector(".loginCredentialPassword").value.length != 0) {
-//         cont = true;
-//     } else {
-//         cont = false;
+    if (document.querySelector(".loginCredentialPassword").value.length != 0) {
+        cont = true;
+    } else {
+        cont = false;
 
-//         passwordErrorShow("Please Enter Password");
-//     }
+        passwordErrorShow("Please Enter Password");
+    }
 
-//     if (cont && document.querySelector(".loginCredentialPassword").value.length != 0 && document.querySelector(".loginCredentialEmail").value.length != 0) {
-//         if (document.querySelector(".loginCredentialEmail").value.split('@').length - 1 == 1 && document.querySelector(".loginCredentialEmail").value.split('.').length - 1 == 1) {
-//             validate();
-//         } else {
-//             cont = false;
+    if (cont && document.querySelector(".loginCredentialPassword").value.length != 0 && document.querySelector(".loginCredentialEmail").value.length != 0) {
+        if (document.querySelector(".loginCredentialEmail").value.split('@').length - 1 == 1 && document.querySelector(".loginCredentialEmail").value.split('.').length - 1 == 1) {
+            validate();
+        } else {
+            cont = false;
 
-//             emailErrorShow("Please Enter a Valid Email")
-//         }
-//     }
-// });
+            emailErrorShow("Please Enter a Valid Email")
+        }
+    }
+});
 
-// Array.prototype.indexOfObject = function (property, value) {
-//     for (let i = 0, len = this.length; i < len; i++) {
-//         if (this[i][property] === value) {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
+function validate() {
+  signInWithEmailPassword();
+}
 
-// function validate() {
-//     var email = document.querySelector(".loginCredentialEmail");
-//     var password = document.querySelector(".loginCredentialPassword");
+function signInWithEmailPassword() {
+  var email = document.querySelector(".loginCredentialEmail").value;
+  var password = document.querySelector(".loginCredentialPassword").value;
 
-//     let checkEmail = users.some((ele) => ele.email === email.value);
+  firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential) => {
+      localStorage.setItem("loggedIn", JSON.stringify(false));
+      localStorage.setItem("rememberMe", JSON.stringify(document.getElementById("rem").checked));
 
-//     if (checkEmail) {
-//         var idx = users.indexOfObject("email", email.value);
+      var user = userCredential.user;
 
-//         if (users[idx].password == password.value) {
-//             localStorage.setItem("sign", JSON.stringify(true));
-//             localStorage.setItem("currentUser", JSON.stringify(idx));
-//             localStorage.setItem("rememberMe", JSON.stringify(document.getElementById("rem").checked))
+      console.log(user)
 
-//             window.location.href = "home.html";
-//         } else {
-//             passwordErrorShow("Incorrect Password");
-//         }
-//     } else {
-//         emailErrorShow("An Account With That Email Doesn't Exist!");
-//     }
-// }
+      // var entity = {
+      //   id: profile.getId(),
+      //   name: profile.getName(),
+      //   image: profile.getImageUrl(),
+      //   email: profile.getEmail(),
+      // }
+    
+      localStorage.setItem("entity", JSON.stringify(user));
+
+      // window.location.href = "home.html";
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      passwordErrorShow(errorMessage);
+    });
+}
+
+function forgotPassword() {
+  const email = alert("Enter Email");
+
+  firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+      alert("Sent!")
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      alert(errorMessage)
+    });
+}
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,138 +1,103 @@
-// if (JSON.parse(localStorage.getItem("sign"))) {
-//     window.location.href = "home.html"
-// }
+if (JSON.parse(localStorage.getItem("loggedIn"))) {
+    window.location.href = "home.html"
+}
 
-// if (localStorage.getItem("users") == null) {
-//     window.location.href = "login.html";
-// } else {
-//     var users = JSON.parse(localStorage.getItem("users"));
-// }
+var cont;
 
-// var cont;
+var emailError = document.querySelector(".errorEmail");
+var passwordError = document.querySelector(".errorPassword");
+var usernameError = document.querySelector(".errorUsername");
+var passwordError2 = document.querySelector(".errorPassword2");
 
-// var emailError = document.querySelector(".errorEmail");
-// var passwordError = document.querySelector(".errorPassword");
-// var usernameError = document.querySelector(".errorUsername");
-// var passwordError2 = document.querySelector(".errorPassword2");
+function emailErrorShow(value) {
+    emailError.style.display = "block";
+    emailError.innerHTML = value;
+}
 
-// function emailErrorShow(value) {
-//     emailError.style.display = "block";
-//     emailError.innerHTML = value;
-// }
+function passwordErrorShow(value) {
+    passwordError.style.display = "block";
+    passwordError.innerHTML = value;
+}
 
-// function passwordErrorShow(value) {
-//     passwordError.style.display = "block";
-//     passwordError.innerHTML = value;
-// }
+function usernameErrorShow(value) {
+    usernameError.style.display = "block";
+    usernameError.innerHTML = value;
+}
 
-// function usernameErrorShow(value) {
-//     usernameError.style.display = "block";
-//     usernameError.innerHTML = value;
-// }
+function passwordErrorShow2(value) {
+    passwordError2.style.display = "block";
+    passwordError2.innerHTML = value;
+}
 
-// function passwordErrorShow2(value) {
-//     passwordError2.style.display = "block";
-//     passwordError2.innerHTML = value;
-// }
+var loginBtn = document.querySelector(".signupButton");
 
-// var loginBtn = document.querySelector(".signupButton");
+loginBtn.addEventListener("click", function () {
+    emailError.style.display = "none";
+    passwordError.style.display = "none";
+    usernameError.style.display = "none";
+    passwordError2.style.display = "none";
 
-// loginBtn.addEventListener("click", function () {
-//     emailError.style.display = "none";
-//     passwordError.style.display = "none";
-//     usernameError.style.display = "none";
-//     passwordError2.style.display = "none";
+    if (document.querySelector(".signupCredentialEmail").value.length != 0) {
+        cont = true;
+    } else {
+        cont = false;
 
-//     if (document.querySelector(".signupCredentialEmail").value.length != 0) {
-//         cont = true;
-//     } else {
-//         cont = false;
+        emailErrorShow("Please Enter Email");
+    }
 
-//         emailErrorShow("Please Enter Email");
-//     }
+    if (document.querySelector(".signupCredentialPassword").value.length != 0) {
+        cont = true;
+    } else {
+        cont = false;
 
-//     if (document.querySelector(".signupCredentialPassword").value.length != 0) {
-//         cont = true;
-//     } else {
-//         cont = false;
+        passwordErrorShow("Please Enter Password");
+    }
 
-//         passwordErrorShow("Please Enter Password");
-//     }
+    if (document.querySelector(".signupCredentialUsername").value.length != 0) {
+        cont = true;
+    } else {
+        cont = false;
 
-//     if (document.querySelector(".signupCredentialUsername").value.length != 0) {
-//         cont = true;
-//     } else {
-//         cont = false;
+        usernameErrorShow("Please Enter Username");
+    }
 
-//         usernameErrorShow("Please Enter Username");
-//     }
+    if (cont && document.querySelector(".signupCredentialPassword").value.length != 0 && document.querySelector(".signupCredentialEmail").value.length != 0) {
+        if (document.querySelector(".signupCredentialEmail").value.split('@').length - 1 == 1 && document.querySelector(".signupCredentialEmail").value.split('.').length - 1 == 1) {
+            validate();
+        } else {
+            cont = false;
 
-//     if (cont && document.querySelector(".signupCredentialPassword").value.length != 0 && document.querySelector(".signupCredentialEmail").value.length != 0) {
-//         if (document.querySelector(".signupCredentialEmail").value.split('@').length - 1 == 1 && document.querySelector(".signupCredentialEmail").value.split('.').length - 1 == 1) {
-//             validate();
-//         } else {
-//             cont = false;
+            emailErrorShow("Please Enter a Valid Email");
+        }
+    }
+});
 
-//             emailErrorShow("Please Enter a Valid Email");
-//         }
-//     }
-// });
+function validate() {
+    var email2 = document.querySelector(".signupCredentialEmail");
+    var password2 = document.querySelector(".signupCredentialPassword");
+    var password3 = document.querySelector(".signupCredentialPassword2");
+    var username2 = document.querySelector(".signupCredentialUsername");
 
-// Array.prototype.indexOfObject = function (property, value) {
-//     for (let i = 0, len = this.length; i < len; i++) {
-//         if (this[i][property] === value) {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-
-// function validate() {
-//     var email2 = document.querySelector(".signupCredentialEmail");
-//     var password2 = document.querySelector(".signupCredentialPassword");
-//     var password3 = document.querySelector(".signupCredentialPassword2");
-//     var username2 = document.querySelector(".signupCredentialUsername");
-
-//     let checkEmail = users.some((ele) => ele.email === email2.value);
-
-//     if (checkEmail == false) {
-//         if (password2.value.length >= 8 && !(password2.value.indexOf(" ") >= 0)) {
-//             if (password3.value == password2.value) {
-
-//                 var newUser = {
-//                     email: email2.value,
-//                     password: password2.value,
-//                     username: username2.value,
-//                 };
-
-//                 users.push(newUser);
-
-//                 localStorage.setItem("users", JSON.stringify(users));
-
-//                 var idx = users.indexOfObject("email", email2.value);
-
-//                 localStorage.setItem("sign", JSON.stringify(true));
-//                 localStorage.setItem("currentUser", JSON.stringify(idx));
-
-//                 Email.send({
-//                     Host: "smtp.gmail.com",
-//                     Username: "slearner321@gmail.com",
-//                     Password: "singh2000",
-//                     To: users[idx].email,
-//                     From: "slearner321@gmail.com",
-//                     Subject: "Welcome!",
-//                     Body: "Welcome to Our Service, " + users[idx].username,
-//                 })
-
-//                 window.location.href = "home.html";
-
-//             } else {
-//                 passwordErrorShow2("Passwords do Not Match")
-//             }
-//         } else {
-//             passwordErrorShow("Please Enter a Valid Password")
-//         }
-//     } else {
-//         emailErrorShow("An Account With That Email Already Exists!");
-//     }
-// }
+    if (password2.value.length >= 8 && !(password2.value.indexOf(" ") >= 0)) {
+        if (password3.value == password2.value) {
+            var email = email2.value;
+            var password = password2.value;
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then((userCredential) => {
+                    var user = userCredential.user;
+                    
+                    // window.location.href = "home.html";
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    
+                    passwordErrorShow2(errorMessage);
+                });
+        } else {
+            passwordErrorShow2("Passwords do Not Match")
+        }
+    } else {
+        passwordErrorShow("Please Enter a Valid Password")
+    }
+}
