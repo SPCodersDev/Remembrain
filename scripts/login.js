fb2f37f5b35d7d4d690dd3eabe21f47b55b5095b
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-07 18:17:27 +0000

    try and decode myself (response)

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,17 @@
 function onSignIn(response) {
 
-    const responsePayload = decodeJwtResponse(response.credential);
+    // const responsePayload = decodeJwtResponse(response.credential);
 
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
+    // console.log("ID: " + responsePayload.sub);
+    // console.log('Full Name: ' + responsePayload.name);
+    // console.log('Given Name: ' + responsePayload.given_name);
+    // console.log('Family Name: ' + responsePayload.family_name);
+    // console.log("Image URL: " + responsePayload.picture);
+    // console.log("Email: " + responsePayload.email);
+
+    var responsePayload = response.getBasicProfile();
+
+    console.log(reponse)
 
     var entity = {
         id: responsePayload.sub,
