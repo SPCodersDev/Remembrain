472233e54c852c827f29d93e45b8fb4f07365937
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-08 13:19:15 +0000

    Settings P3

--- scripts/settings.js
+++ scripts/settings.js
@@ -11,7 +11,7 @@ function showError2(error) {
     $("#cpe2").html(error);
 }
 
-$("#cpBtn").on("click", function() {
+$("#cpBtn").on("click", function () {
     if (document.getElementById("cp1").value.length == 0) {
         showError1("Please Enter Password");
     }
@@ -23,12 +23,21 @@ $("#cpBtn").on("click", function() {
     if (document.getElementById("cp1").value.length != 0 && document.getElementById("cp2").value.length != 0) {
         if (document.getElementById("cp1").value.length >= 8 && !(document.getElementById("cp1").value.indexOf(" ") >= 0)) {
             if (document.getElementById("cp2").value == document.getElementById("cp1").value) {
-                localStorage.setItem("users", JSON.stringify(users));
+                const user = firebase.auth().currentUser;
+                const newPassword = document.getElementById("cp1").value = "";
+
+                user.updatePassword(newPassword).then(() => {
+                    document.querySelector(".popup2").style.display = "flex";
+                    window.setTimeout(function () {
+                        document.querySelector(".popup2").style.display = "none";
+                        $(".overlay").hide();
+                    }, 5000);
+                }).catch((error) => {
+                    showError2(error)
+                });
 
                 document.getElementById("cp1").value = "";
                 document.getElementById("cp2").value = "";
-
-                changed();
             } else {
                 showError2("Passwords do Not Match")
             }
