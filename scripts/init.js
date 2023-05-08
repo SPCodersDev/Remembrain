380a1707326d7b38b0961608d0eaf92e59586d45
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-08 09:29:27 +0100

    Move Firebase to New Account

--- scripts/init.js
+++ scripts/init.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBeUGKkDd4CpYQrzeSJF_gEQEHDlsQIGg0",
-    authDomain: "programmingcompetition2023.firebaseapp.com",
-    databaseURL: "https://programmingcompetition2023-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "programmingcompetition2023",
-    storageBucket: "programmingcompetition2023.appspot.com",
-    messagingSenderId: "482409458124",
-    appId: "1:482409458124:web:c6472678e76fbd2583d569"
+  apiKey: "AIzaSyCKlV4dAhx748lk8bp62fEm5Aq7dE3LEYc",
+  authDomain: "remembrain-1ad7c.firebaseapp.com",
+  databaseURL: "https://remembrain-1ad7c-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "remembrain-1ad7c",
+  storageBucket: "remembrain-1ad7c.appspot.com",
+  messagingSenderId: "162216838424",
+  appId: "1:162216838424:web:65394f894cd45047440a6d",
+  measurementId: "G-3SMBTZ4S01"
 };
 
 firebase.initializeApp(firebaseConfig);
