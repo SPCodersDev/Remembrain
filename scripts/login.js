3ba99abcfaed67389c245b75d9a0f2e751955ad3
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-09 09:21:50 +0000

    allow enter press - login

--- scripts/login.js
+++ scripts/login.js
@@ -21,6 +21,15 @@ var loginBtn = document.querySelector(".loginButton");
 
 loginBtn.addEventListener("click", function() { loginFunction() });
 
+document.querySelector('input').onkeypress = function(e){
+  if (!e) e = window.event;
+  var keyCode = e.code || e.key;
+  if (keyCode == 'Enter'){
+    loginFunction();
+    return false;
+  }
+}
+
 function loginFunction() {
   emailError.style.display = "none";
   passwordError.style.display = "none";
