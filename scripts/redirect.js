b028356d6560c1bbf9a96fb270a2e95c8d8e8d9b
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-10 07:34:22 +0000

    fix redirect?

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,21 +1,25 @@
-if (localStorage.loggedIn == null) {
-    localStorage.setItem("loggedIn", JSON.stringify(false));
-}
-
-if (window.location.href.includes("login.html") || window.location.href.includes("signup.html")) {
-    if (JSON.parse(localStorage.loggedIn)) {
-        window.location.href = "home.html";
+if(!window.location.href.includes(".html")) {
+    window.location.href = "index.html";
+} else {
+    if(localStorage.loggedIn == null) {
+        localStorage.setItem("loggedIn", JSON.stringify(false));
     }
-} else if (window.location.href.includes("home.html") || window.location.href.includes("settings.html")) {
-    if (!window.location.href.includes("5500") && !window.location.href.includes(".preview.app")) {
-        if (!JSON.parse(localStorage.loggedIn)) {
+
+    if (window.location.href.includes("login.html") || window.location.href.includes("signup.html")) {
+        if (JSON.parse(localStorage.loggedIn)) {
+            window.location.href = "home.html";
+        } 
+    } else if (window.location.href.includes("home.html") || window.location.href.includes("settings.html")) {
+        if (!window.location.href.includes("5500") && !window.location.href.includes(".preview.app")) {
+            if (!JSON.parse(localStorage.loggedIn)) {
+                window.location.href = "login.html";
+            }
+        }
+    } else if(!window.location.href.includes("index.html")) {
+        if (JSON.parse(localStorage.loggedIn)) {
+            window.location.href = "home.html";
+        } else {
             window.location.href = "login.html";
         }
-    }
-} else if(!window.location.href.includes("index.html") || window.location.href[window.location.href.length - 1] != "/") {
-    if (JSON.parse(localStorage.loggedIn)) {
-        window.location.href = "home.html";
-    } else {
-        window.location.href = "login.html";
     }
 }
