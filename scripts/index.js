5e0dc1a03dba2c936556a10e2e57114c03846bc5
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-05-16 07:35:35 +0000

    Change Navbar Height & JS Index Navbar

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,44 @@ $(".getStarted").on("click", function() {
         window.location.href = "signup.html";
     }
 });
+
+var topBarRight = document.querySelector(".topBarRight");
+
+topBarRight.innerHTML = "";
+
+if(localStorage.loggedIn == null || !JSON.parse(localStorage.loggedIn)) {
+    var topBarRightLink = document.createElement("div");
+    topBarRightLink.classList.add("topBarRightLink");
+
+    var topBarLogin = document.createElement("a");
+    topBarLogin.href = "login.html";
+    topBarLogin.classList.add("topBarLogin");
+    topBarLogin.innerHTML = "Log In";
+
+    topBarRightLink.appendChild(topBarLogin);
+
+    topBarRight.appendChild(topBarRightLink);
+
+    var topBarRightLink = document.createElement("div");
+    topBarRightLink.classList.add("topBarRightLink");
+    
+    var topBarSignup = document.createElement("a");
+    topBarSignup.href = "signup.html";
+    topBarSignup.classList.add("topBarSignup");
+    topBarSignup.innerHTML = "Sign Up"
+
+    topBarRightLink.appendChild(topBarSignup);
+
+    topBarRight.appendChild(topBarRightLink);
+} else {
+    var topBarRightLink = document. createElement ("div");
+    topBarRightLink.classList.add("topBarRightLink");
+
+    var topBarHome = document. createElement ("a");
+    topBarHome.href = "home.html";
+    topBarHome.classList.add("topBarLogin");
+    topBarHome. innerHTML = "Home"
+    
+    topBarRightLink.appendChild(topBarHome);
+    topBarRight.appendChild(topBarRightLink) ;
+}
