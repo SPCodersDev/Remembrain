d85bd96b64b8b4d244eb7045c4f39b0e6c3ed8e7
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2023-07-05 19:23:42 +0000

    Memory Match P1

---
+++ games/Memory Match/memoryMatch.js
@@ -0,0 +1,102 @@
+const cards = document.querySelectorAll(".card"),
+timeTag = document.querySelector(".time b"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button");
+
+let maxTime = 0;
+let timeLeft = maxTime;
+let flips = 0;
+let matchedCard = 0;
+let disableDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
+
+function initTimer() {
+    timeTag.style.color = "black";
+    //if(timeLeft <= 0) {
+        //timeTag.innerText = "GAME OVER";
+        //timeTag.style.color = "red";
+        //return clearInterval(timer);
+    //}
+    timeLeft = timeLeft + 0.1;
+    let num = String(timeLeft)
+    let arr = num.split(".");
+    let newSecond = arr[1][0]
+    let final = arr[0]+newSecond
+    timeTag.innerText = parseFloat(final/10);
+}
+
+function flipCard({target: clickedCard}) {
+    if(!isPlaying) {
+        isPlaying = true;
+        timer = setInterval(initTimer, 100);
+    }
+    if(clickedCard !== cardOne && !disableDeck) {
+        flips++;
+        flipsTag.innerText = flips;
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view object").src,
+        cardTwoImg = cardTwo.querySelector(".back-view object").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matchedCard++;
+        if(matchedCard == 6) {
+            return clearInterval(timer);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+
+function shuffleCard() {
+    timeLeft = maxTime;
+    flips = matchedCard = 0;
+    cardOne = cardTwo = "";
+    clearInterval(timer);
+    timeTag.innerText = timeLeft;
+    flipsTag.innerText = flips;
+    disableDeck = isPlaying = false;
+
+    let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+
+    cards.forEach((card, index) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector(".back-view object");
+        setTimeout(() => {
+            imgTag.data = `/games/Memory Match/images/img-${arr[index]}.svg`;
+        }, 500);
+        card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCard();
+
+refreshBtn.addEventListener("click", shuffleCard);
+
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
